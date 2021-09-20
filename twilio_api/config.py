4f904c330366705fe198abaded5394f63e8e9625
kyte <kyteinsky@gmail.com>
2021-09-20 08:02:59 +0530

    init

---
+++ twilio_api/config.py
@@ -0,0 +1,10 @@
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
